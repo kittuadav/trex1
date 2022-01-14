d4ecba8821866707d29a8a570cb4bac65fe42320
kittuadav <97619726+kittuadav@users.noreply.github.com>
2022-01-14 21:34:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+   //console.log(marks[3]);
+  // console.log(marks[6]);
+   //console.log(marks[8]);
+   for(var i=0;i<marks.length;i++){
+  if(marks[i]>=45){
+    console.log(marks[i])
+  }
+   }
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+
+
